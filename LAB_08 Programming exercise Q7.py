7b8f6dec4b84a7fffcc2763613fb4d480b767800
SarimAliKhan <45090042+SarimAliKhan@users.noreply.github.com>
2019-01-05 22:19:18 +0500

    Add files via upload

---
+++ LAB_08 Programming exercise Q7.py
@@ -0,0 +1,9 @@
+print('SARIM ALI KHAN')
+print('18b-043-CS')
+print('Lab # 08')
+print ('12/28/2018')
+print ('Question 7')
+def cheers(n):
+    again_string =' '.join(n)
+    print ('How do you spell winner?\nI know!, I know!\n',again_string.upper(),'\nAnd that\'s how you spell winner','\n Go', n.title(),'!')
+cheers('uitians')
